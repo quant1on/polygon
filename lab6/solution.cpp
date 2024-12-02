d3947cd60b66f763b6492f31781e6f0b2190f012
quant1on <ilyusha.k06@gmail.com>
2024-12-03 01:13:08 +0300

    feat: added report for lab5 + solved lab6

--- lab6/solution.cpp
+++ lab6/solution.cpp
@@ -1,23 +1,87 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stack>
 using namespace std;
+
+int RectangleArea(int height[], int n){
+    pair<int, int> limits[n];
+    stack<int> dp;
+    int i = 0, max_area = 0;
+
+    while (i < n){
+        if (dp.empty() || height[dp.top()] <= height[i]){
+            dp.push(i);
+            i++;
+        }
+        else{
+            limits[dp.top()].second = i - 1;
+            dp.pop();
+        }
+    }
+
+    while (!dp.empty()){
+        limits[dp.top()].second = n - 1;
+        dp.pop();
+    }
+
+    i--;
+
+    while (i >= 0){
+        if (dp.empty() || height[dp.top()] <= height[i]){
+            dp.push(i);
+            i--;
+        }
+        else{
+            limits[dp.top()].first = i + 1;
+            dp.pop();
+        }
+    }
+
+    while (!dp.empty()){
+        limits[dp.top()].first = 0;
+        dp.pop();
+    }
+
+    int area;
+    for (int j = 0; j < n; j++){
+        area = height[j] * (limits[j].second - limits[j].first + 1);
+        max_area = max(max_area, area);
+    }
+
+    return max_area;
+}
+
+
 
 int maximalRectangle(vector<vector<char>>& matrix){
     int clmns = matrix[0].size(), rows = matrix.size(), ans = 0;
-    vector<int> height(clmns, 0);
-    for (int i = 0; i < matrix.size(); i++){
-        for (int j = 0; j < rows; j++){
-            if (matrix[i][j] == 0) height[j] = 0;
+
+    int height[clmns];
+    for (int i = 0; i < clmns; i++) height[i] = 0;
+
+    for (int i = 0; i < rows; i++){
+        
+        for (int j = 0; j < clmns; j++){
+            if (matrix[i][j] == '0') height[j] = 0;
             else height[j]++;
         }
 
-        ans = max(ans, RectangleArea(height));
+        ans = max(ans, RectangleArea(height, clmns));
     }
 
     return ans;
 }
 
-int RectangleArea(vector<int> &height){
-    
+
+int main(){
+    vector<vector<char>> test;
+    test = {
+        {'1', '0', '1', '0', '0'},
+        {'1', '0', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'},
+        {'1', '0', '0', '1', '0'}
+    };
+
+    cout << maximalRectangle(test);
 }
